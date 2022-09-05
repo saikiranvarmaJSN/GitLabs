d32c0b12910c8935303dd0a38b3145c4afcea46a
saikiranvarmaJSN <89576203+saikiranvarmaJSN@users.noreply.github.com>
2022-09-05 23:24:22 +0530

    Revert "commit seven"
    This reverts commit ee3e02bd0ff60ecda58a7848ef6b9770bd474320.

--- hello.py
+++ hello.py
@@ -2,8 +2,4 @@ name = "Saikiran Varma Vegesana"
 print("Hello world",name)
 if name: # Checking if name is provided
 	print(True)
-
 print("my name is don srinu")
-=======
-print("Hello world",name)
->>>>>>> parent of ee3e02b (commit six)
